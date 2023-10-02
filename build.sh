168bdd167d8817622c41ee5311f2c3b354a0d188
BostoenToby <toby.bostoen123@gmail.com>
2023-10-02 14:30:11 +0200

    update package.json & build for package

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 # If this is your first time publishing, you will need to create an account with npmjs.com
 # run pnpm adduser (npm adduser if you are using npm) and follow the prompts
 rm -fr ./package
+pnpm run prepublishOnly
 pnpm run package
 pnpm publish ./package
