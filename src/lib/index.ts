3bc8e4dfb0a750a57ea1f93f4d8b6338dc706181
BostoenToby <toby.bostoen123@gmail.com>
2024-01-04 11:35:56 +0100

    bump to 0.0.24 remove obsolete code in datatype

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 //Copyright 2023 RADar-AZDelta
 //SPDX-License-Identifier: gpl3+
 import Search from '$lib/components/Search.svelte'
-import type { RowSelectedEventDetail } from '$lib/Types'
+import type { RowSelectedEventDetail, IView } from '$lib/Types'
 
-export { Search, RowSelectedEventDetail }
+export { Search }
+export type { RowSelectedEventDetail, IView }
--- src/lib/utilClasses/AthenaDataTypeImpl.ts
+++ src/lib/utilClasses/AthenaDataTypeImpl.ts
@@ -4,29 +4,12 @@ import { DataTypeCommonBase } from '@radar-azdelta/svelte-datatable'
 import type {
   TFilter,
   IDataTypeFunctionalities,
-  IColumnMetaData,
-  IDataTypeInfo,
   IRender,
   SortDirection,
   FetchDataFunc,
 } from '@radar-azdelta/svelte-datatable'
 
 export class AthenaDataTypeImpl extends DataTypeCommonBase implements IDataTypeFunctionalities {
-  async setData(dataTypeInfo: IDataTypeInfo): Promise<void> {
-    const { data, internalOptions, internalColumns, renderedData } = dataTypeInfo
-    this.data = data
-    this.internalOptions = internalOptions
-    this.internalColumns = internalColumns
-    this.renderedData = renderedData
-  }
-
-  async setInternalColumns(columns: IColumnMetaData[] | undefined): Promise<IColumnMetaData[]> {
-    if (columns) this.internalColumns = columns
-    if (!this.internalColumns) return []
-    for (let col of this.internalColumns) if (!col.width) col.width = this.internalOptions!.defaultColumnWidth
-    return this.internalColumns
-  }
-
   async render(): Promise<IRender> {
     const filteredColumns = this.internalColumns!.reduce<Map<string, TFilter>>((acc, cur) => {
       if (cur && cur.filter) acc.set(cur.id, cur.filter)
@@ -40,7 +23,6 @@ export class AthenaDataTypeImpl extends DataTypeCommonBase implements IDataTypeF
     const originalIndices = Array.from({ length: results.data.length }, (_, i) => i)
     const totalRows = results.totalRows
     this.renderedData = results.data
-
     return { originalIndices, totalRows, renderedData: this.renderedData, internalColumns: this.internalColumns }
   }
 }
